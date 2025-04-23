12710fdf4cdc7e71e025578e4e67650ba4e3869b
VictorPersona <gauravtupe00@gmail.com>
2025-04-23 08:39:38 +0530

    Add round-robin scheduler and resource usage visualization

--- sim.py
+++ sim.py
@@ -3,12 +3,21 @@ from tenant import Tenant
 def test_tenant_class():
     tenant_1 = Tenant("Tenant A",50,1024,100)
     tenant_2 = Tenant("Tenant B",30,512,200)
+    tenant_3 = Tenant("Tenant_C", 70, 2048, 300)
 
-    print(tenant_1)
-    print(tenant_2)
+    tenants = [tenant_1,tenant_2,tenant_3]
 
-    tenant_1.simulate_workload()
-    tenant_2.simulate_workload()
+    print("Starting round-robin scheduler...\n")
+    round_robin_scheduler(tenants)
+
+
+def round_robin_scheduler(tenants):
+    print("\nScheduling tenants...")
+
+    for tenant in tenants:
+        print(f"Allocating resources to {tenant.name}:")
+        tenant.simulate_workload()
+        print(f"{tenant.name} - CPU :{tenant.cpu_demand}%, Mem: {tenant.memory_demand}MB\n")
 
 if __name__ == "__main__":
     test_tenant_class()
---
+++ visualize.py
@@ -0,0 +1,26 @@
+import matlplotlib.pyplot as plt
+
+def plot_resource_usage(cpu_usages,mem_usages,lables):
+    x = range(len(labels))
+    plt.figure(figsize=(10,5))
+
+    plt.subplot(1,2,1)
+
+    plt.bar(x,cpu_usages, color='skyblue')
+    plt.xticks(x,labels,rotation=45)
+
+    plt.title("CPU Usage (%)")
+    plt.xlabel("Tenant")
+    plt.ylabel("CPU %")
+
+    plt.subplot(1,2,2)
+    plt.bar(x,mem_usages,color="salmon")
+
+    plt.xticks(x,labels,rotation=45)
+    plt.title("Memory Usage (MB)")
+    plt.xlabel("Tenant")
+    plt.ylabel("Memory")
+
+    plt.tight_layout()
+
+    plt.show()
